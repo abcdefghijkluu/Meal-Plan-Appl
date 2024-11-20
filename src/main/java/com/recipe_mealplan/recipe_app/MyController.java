443a30ba0e5dda372741e02b95687145a8fe1fc9
Robertgon03 <robertgonzalez141@gmail.com>
2024-11-19 22:15:23 -0500

    getting html page to work with springboot

---
+++ src/main/java/com/recipe_mealplan/recipe_app/MyController.java
@@ -0,0 +1,26 @@
+package com.recipe_mealplan.recipe_app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+
+
+@Controller
+public class MyController {
+
+    // Home page route (default route)
+    @GetMapping("/")
+    public String homePage() {
+        return "home"; // This should render home.html
+    }
+
+    // Login page route
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login"; // This should render login.html
+    }
+}
+
+
