1400f15536d395e0292408f13ede117f856ec294
Robertgon03 <robertgonzalez141@gmail.com>
2024-11-29 22:20:47 -0500

    adding the recipe page

--- src/main/java/com/recipe_mealplan/Controllers/RecipeController.java
+++ src/main/java/com/recipe_mealplan/Controllers/RecipeController.java
@@ -1,5 +1,26 @@
 package com.recipe_mealplan.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import com.recipe_mealplan.Repsoitory.RecipeRepository;
+import lombok.var;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
 public class RecipeController {
+    @Autowired
+    private RecipeRepository reciperepo;
+    @GetMapping("recipes/index")
+    public String recipesPage(Model model) {
+        var recipes = reciperepo.findAll();
+        model.addAttribute("recipes", recipes);
+        return "recipes/index";
+    }
+    
+
 
 }
