cb5d337a9db775d21df78882391a206dd1c81536
Robertgon03 <robertgonzalez141@gmail.com>
2024-12-07 16:14:47 -0500

    adding files for advanced feature

--- src/main/java/com/recipe_mealplan/Controllers/GroceryListController.java
+++ src/main/java/com/recipe_mealplan/Controllers/GroceryListController.java
@@ -1,5 +1,31 @@
 package com.recipe_mealplan.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import com.recipe_mealplan.service.GroceryListService;
+import com.recipe_mealplan.Models.RecipeIngredientDTO;
+import com.recipe_mealplan.Models.MealDetailsDTO;
+
+
+
+import java.util.List;
+
+@Controller
+ // Ensure your base URL is set here
 public class GroceryListController {
 
+    @Autowired
+    private GroceryListService groceryListService;
+
+    @GetMapping("/recipes/List{mealPlanId}")
+    public String getGroceryList(@PathVariable Long mealPlanId, Model model) {
+        List<RecipeIngredientDTO> groceryList = groceryListService.getGroceryListForMealPlan(mealPlanId);  // Pass dynamic mealPlanId
+        model.addAttribute("groceryList", groceryList);
+        return "recipes/List";
+
+    
+}
 }
